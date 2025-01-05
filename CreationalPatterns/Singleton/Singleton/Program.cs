8e3d68ce31a992b7deb32e1771a04b27d917b4dd
IgorSantanaM <you@example.com>
2025-01-05 19:46:09 -0400

    (feat): Added the singleton design patterns

---
+++ CreationalPatterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Singleton singleton1 = Singleton.Instance;
+Singleton singleton2 = Singleton.Instance;
+
+sealed class Singleton 
+{
+    private static Lazy<Singleton> _lazyInstance = new(() => new());
+
+    public static Singleton Instance => _lazyInstance.Value;
+
+    private Singleton()
+    {
+        Console.WriteLine("Creating the instance.");
+    }
+}

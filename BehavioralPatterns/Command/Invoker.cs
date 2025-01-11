8fd3cb4fcb56b8339858be31de30fb3e70771678
IgorSantanaM <you@example.com>
2025-01-11 14:13:28 -0400

    (feat): Added command

---
+++ BehavioralPatterns/Command/Invoker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class Invoker
+    {
+        public readonly List<ICommand?> _commands = [];
+        public readonly Stack<ICommand?> _undoStack = [];
+
+        public void AddCommand(ICommand command)
+        {
+            _commands.Add(command);
+        }
+
+        public void UndoLastCommand() 
+        {
+            var command = _undoStack.Pop();
+            command?.Undo();
+        }
+
+        public void ExecuteCommands()
+        {
+            foreach (var command in _commands)
+            {
+                command?.Execute();
+                _undoStack.Push(command);
+            }
+
+            _commands.Clear();
+        }
+    }
+}
---
+++ BehavioralPatterns/Command/Receiver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class Receiver
+    {
+        public void Action(string message)
+        {
+            Console.WriteLine($"Receiver Invoked: {message}");
+        }
+
+        public void UndoAction(string message)
+        {
+            Console.WriteLine($"Receiver undoing: {message}");
+        }
+    }
+}

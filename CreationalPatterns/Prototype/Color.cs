dd6bb4c32259d48df3629890db69a3b2b67be345
IgorSantanaM <you@example.com>
2025-01-10 23:10:09 -0400

    (feat): Added prototype

---
+++ CreationalPatterns/Prototype/Color.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Prototype
+{
+    public class Color(ushort red, ushort green, ushort blue) : IPrototype<Color>
+    {
+        public static readonly Color LightGray = new(217, 217, 217);
+
+        public Color Clone()
+        {
+            return new Color(red, green, blue);
+        }
+    }
+}

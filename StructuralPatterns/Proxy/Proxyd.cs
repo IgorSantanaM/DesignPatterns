9636800d03b81b7d0e33cdbed2ae992fd1e00b88
IgorSantanaM <you@example.com>
2025-01-13 18:34:57 -0400

    (feat): Added Proxy

---
+++ StructuralPatterns/Proxy/Proxyd.cs
@@ -0,0 +1,26 @@
+﻿namespace Proxy
+{
+    public class Proxy : Subject
+    {
+        private RealSubject? _realSubject = null;
+        public Proxy()
+        {
+            Console.WriteLine("Initiating Proxy");
+        }
+        public void Operation()
+        {
+            Console.WriteLine("Peforming operation on Proxy");
+
+            if(_realSubject is null)
+            {
+                Console.WriteLine("Real subject is null, creating...");
+
+                _realSubject = new RealSubject();
+            }
+            Console.WriteLine("Logging before the operation");
+            _realSubject.Operation();
+            Console.WriteLine("Logging after the operation");
+        }
+
+    }
+}
---
+++ StructuralPatterns/Proxy/RealSubject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class RealSubject : Subject
+    {
+
+        public RealSubject()
+        {
+            Console.WriteLine("Initiating real subject");
+        }
+        public void Operation()
+        {
+            Console.WriteLine("Peforming operation on real subject");
+        }
+
+    }
+}

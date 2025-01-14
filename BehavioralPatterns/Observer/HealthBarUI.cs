84745231ebee9d73a6c6ea2d33c2a424b124bdf1
IgorSantanaM <you@example.com>
2025-01-14 12:01:56 -0400

    (feat): Added Observer

---
+++ BehavioralPatterns/Observer/HealthBarUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class HealthBarUI(Player player) : IGameObserver
+    {
+        private readonly Player _player = player;
+        public void Update()
+        {
+            var health = _player.GetHealth();
+
+            Console.WriteLine($"player's health: {health}");
+        }
+    }
+}
---
+++ BehavioralPatterns/Observer/ScoreUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class ScoreUI(Player player) : IGameObserver
+    {
+
+        private readonly Player _player = player;
+        public void Update()
+        {
+            var score = _player.GetScore();
+
+            Console.WriteLine($"player's score: {score}");
+        }
+    }
+}

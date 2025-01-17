fb623b06ee4efcad1e159d45e27e72d218eecf37
IgorSantanaM <you@example.com>
2025-01-16 22:02:01 -0400

    (feat): Added Composite

---
+++ StructuralPatterns/Composite/LearningResourse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public abstract class LearningResourse
+    {
+        public abstract string GetName();
+        public abstract decimal GetPrice();
+        public abstract TimeSpan GetDurantion();
+        public virtual void Add(LearningResourse learningResourses) { }
+        public virtual void Remove(LearningResourse learningResourses) { }
+
+        public virtual LearningResourse? GetLearningResourse(string name)
+        {
+            return null;
+        }
+    }
+}
---
+++ StructuralPatterns/Composite/Program.cs
@@ -0,0 +1,18 @@
+﻿using Composite;
+
+LearningResourse root = new Bundle(name: "Zero to hero Clean Architecture");
+
+LearningResourse leafA = new Course(name: "Getting Started with clean architecture",
+    duration: TimeSpan.FromHours(3),
+    price: 100);
+
+LearningResourse leafB = new Course(name: "Depp dive clean architecture",
+    duration: TimeSpan.FromHours(4),
+    price: 120);
+
+root.Add(leafA);
+root.Add(leafB);
+
+Console.WriteLine(root.GetPrice());
+
+Console.WriteLine(root.GetDurantion());

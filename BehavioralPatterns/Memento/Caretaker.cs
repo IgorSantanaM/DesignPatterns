b0e8101a6efc62b9617d1cb4355de90f1b0e6a01
IgorSantanaM <you@example.com>
2025-01-17 21:58:29 -0400

    (feat): Added Memento

---
+++ BehavioralPatterns/Memento/Caretaker.cs
@@ -0,0 +1,19 @@
+﻿using MementoPattern;
+
+namespace Memento
+{
+    public class Caretaker
+    {
+        private readonly List<Originator.Memento> _mementos = [];
+
+        public void AddMemento(Originator.Memento memento)
+        {
+            _mementos.Add(memento);
+        }
+
+        public Originator.Memento GetMemento(int index)
+        {
+            return _mementos[index];
+        }
+    }
+}
---
+++ BehavioralPatterns/Memento/Originator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    public class Originator
+    {
+        private string _state = string.Empty;
+
+        public void SetState(string state)
+        {
+            _state = state;
+        }
+
+        public string GetState()
+        {
+            return _state;
+        }
+        public class Memento
+        {
+            private readonly string _state;
+
+            internal Memento(string state)
+            {
+                _state = state;
+            }
+
+            internal string GetState()
+            {
+                return _state;
+            }
+        }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(_state);
+        }
+
+        public void Restore(Memento memento)
+        {
+            _state = memento.GetState();
+        }
+    }
+}

5d6064b23df46c984a86eb3ad5d40706055046ac
IgorSantanaM <you@example.com>
2025-01-18 23:12:33 -0400

    (feat): Added Bridge

---
+++ StructuralPatterns/Bridge/Abstractions/RefinedAbstraction2.cs
@@ -0,0 +1,19 @@
+﻿using Bridge.Implementors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Abstractions
+{
+    public class RefinedAbstraction2(Implementor implementor) : Abstraction(implementor)
+    {
+        public override void Foo()
+        {
+            Console.WriteLine("------2");
+            Implementor.Print();
+            Console.WriteLine("------2");
+        }
+    }
+}
---
+++ StructuralPatterns/Bridge/Program.cs
@@ -0,0 +1,8 @@
+﻿using Bridge;
+using Bridge.Abstractions;
+using Bridge.Implementors;
+
+Abstraction abstraction = new RefinedAbstraction1(
+    implementor: new ConcreteImplementor2());
+
+abstraction.Foo();

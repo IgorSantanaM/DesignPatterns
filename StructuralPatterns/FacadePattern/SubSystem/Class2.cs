a18d7139db6df10aafa6844840cc3f7859186e17
IgorSantanaM <you@example.com>
2025-01-20 21:52:07 -0400

    (feat): Added Facade

---
+++ StructuralPatterns/FacadePattern/SubSystem/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern.SubSystem
+{
+    public class Class2
+    {
+        public void Operation3()
+        {
+            Console.WriteLine("operation3");
+        }
+    }
+}
---
+++ StructuralPatterns/FacadePattern/SubSystem/Class3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern.SubSystem
+{
+    public class Class3(Class2 class2)
+    {
+        public void Operation4(Class1 class1)
+        {
+            class2.Operation3();
+            class1.Operation2();   
+        }
+    }
+}

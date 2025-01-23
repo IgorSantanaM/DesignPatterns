2960301da0e94b0ec801199c90bea0e7e1ed12c4
IgorSantanaM <you@example.com>
2025-01-22 21:25:20 -0400

    (feat): Added Flyweight

---
+++ StructuralPatterns/FlyWeight/ConcreteFlyweight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyWeight
+{
+    public class ConcreteFlyweight(string instrisicState) : Flyweight
+    {
+        public readonly string _intrisicState = instrisicState;
+        public void Operation(string extrinsicState)
+        {
+            Console.WriteLine($"Concrete flyweight: Instrisic State: {_intrisicState}, Extrinsic State: {extrinsicState}");
+        }
+    }
+}
---
+++ StructuralPatterns/FlyWeight/FlyweightFactory.cs
@@ -0,0 +1,18 @@
+﻿using Key = string;
+namespace FlyWeight
+{
+    public class FlyweightFactory
+    {
+        public readonly Dictionary<Key, Flyweight> _flyweights = [];
+
+        public Flyweight GetFlyweight(Key key)
+        {
+            if (!_flyweights.ContainsKey(key))
+            {
+                _flyweights[key] = new ConcreteFlyweight(instrisicState: key);
+            }
+
+            return _flyweights[key];
+        }
+    }
+}

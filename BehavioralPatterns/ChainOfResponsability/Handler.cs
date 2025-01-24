a6d1d0601a1521dd5156d264667e9899fb68879b
IgorSantanaM <you@example.com>
2025-01-23 23:03:10 -0400

    (feat): Added Chain Of Responsability

---
+++ BehavioralPatterns/ChainOfResponsability/Handler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsability
+{
+    public abstract class Handler
+    {
+        protected Handler? _successor = null;
+
+        public void SetSuccessor(Handler successor)
+        {
+            _successor = successor;
+        }
+
+        public abstract void Handle(string request);
+    }
+}

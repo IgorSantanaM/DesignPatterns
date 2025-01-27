c561d68e28a2294b85fdfc1d3f3b135b2080d031
IgorSantanaM <you@example.com>
2025-01-26 20:13:54 -0400

    (feat): Added Visitor

---
+++ BehavioralPatterns/Visitor/ConcreteElement1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public class ConcreteElement1 : Element
+    {
+        public void Accept(Visitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public string Operation1()
+        {
+            return "Concrete Element 1";
+        }
+    }
+}
